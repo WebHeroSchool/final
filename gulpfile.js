76d98191d0c35db59e4b1857581fdd25b9affdd2
Timur <Mokoto1991@mail.ru>
2019-02-14 14:38:35 +0300

    map

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,19 @@ const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('hello', () => console.log(Date()));
 
 gulp.task('taskjs', () => {
     return gulp.src(['*.js'])
+    .pipe(sourcemaps.init())
     .pipe(concat(['index.js'])
     .pipe(babel({
         presets: ['@babael/env']
     }))
     .pipe(uglify())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('js'));
 });
 
@@ -20,7 +23,13 @@ gulp.task('taskjs', () => {
 
 gulp.task('taskcss', () => {
     return gulp.src(['*.css'])
+    .pipe(sourcemaps.init())
     .pipe(concat(['index.js'])
     .pipe(cssnano())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('css'));
 });
+
+
+
+gulp.task('default', ['taskjs', 'taskcss'])
