7f38ceaccd070675069558e48e878a07b1d7f662
Timur <Mokoto1991@mail.ru>
2019-02-14 07:41:02 +0300

    task

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,13 @@
 var gulp = require('gulp');
 
-gulp.task('hello', () => console.log("Я я закончил задачу"));
+gulp.task('hello', () => console.log(Date()));
+
+gulp.task('taskjs', () => {
+    return gulp.src(['scripts.js'])
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('taskcss', () => {
+    return gulp.src(['styles.css'])
+    .pipe(gulp.dest('css'));
+});
