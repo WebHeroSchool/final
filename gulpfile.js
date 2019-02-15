b36066d0a273e990203854fc46a863aebc49f266
Timur <Mokoto1991@mail.ru>
2019-02-15 13:21:00 +0300

    browser

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,62 @@ const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const cssnano = require('gulp-cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('gulp-browser-sync').create();
 
-
-
+const paths = {
+    scr: {
+        styles: 'styles',
+        scripts: 'scripts'
+    },
+    bulding: {
+        styles: 'styles',
+        scripts: 'scripts'
+    },
+    buildNames: {
+        styles: 'index.min.css',
+        scripts: 'index.min.js'
+    }
+}
 
 gulp.task('hello', () => console.log(Date()));
 
 gulp.task('taskjs', () => {
-    return gulp.src(['*.js'])
-    .pipe(concat(['index.js'])
+    return gulp.src([paths.scr.scripts])
+    .pipe(sourcemaps.init())
+    .pipe(concat([paths.buildNames.scripts])
     .pipe(babel({
         presets: ['@babael/env']
     }))
     .pipe(uglify())
-    .pipe(gulp.dest('js'));
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.bulding.scripts));
 });
 
 gulp.task('taskcss', () => {
-    return gulp.src(['*.css'])
-    .pipe(concat(['index.css'])
+    return gulp.src([paths.scr.styles])
+    .pipe(sourcemaps.init())
+    .pipe(concat([paths.buildNames.styles])
     .pipe(cssnano())
-    .pipe(gulp.dest('css'));
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.bulding.styles));
 });
 
+gulp.task('default', ['taskjs', 'taskcss'])
+
+
+gulp.task('browser-sync', () => {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+    gulp.watch(paths.scr.scripts, ['taskjs-watch']);
+    gulp.watch(paths.scr.styles, ['taskcss-watch']);
+})
+
+
+
+gulp.task('taskjs-watch', ['js'], () => browserSync.reload());
+
+gulp.task('taskcss-watch', ['css'], () => browserSync.reload());
