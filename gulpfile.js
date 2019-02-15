d641b10c17f7a58fd8223159672b3e76f65d552f
Timur <Mokoto1991@mail.ru>
2019-02-15 16:07:45 +0300

    browser

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,20 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
+const gulpif = require('gulp-if');
 const cssnano = require('gulp-cssnano');
 const sourcemaps = require('gulp-sourcemaps');
-const browserSync = require('gulp-browser-sync').create();
+const browserSync = require('browser-sync').create();
+const env = require('gulp-env');
 
 const paths = {
     scr: {
-        styles: 'styles',
-        scripts: 'scripts'
+        styles: 'src/styles/*.css',
+        scripts: 'scr/scripts/*.js'
     },
     bulding: {
-        styles: 'styles',
-        scripts: 'scripts'
+        styles: 'build/styles',
+        scripts: 'build/scripts'
     },
     buildNames: {
         styles: 'index.min.css',
@@ -21,30 +23,33 @@ const paths = {
     }
 }
 
-gulp.task('hello', () => console.log(Date()));
+env ({
+    file: '.env',
+    type: 'ini',
+});
 
-gulp.task('taskjs', () => {
+gulp.task('js', () => {
     return gulp.src([paths.scr.scripts])
     .pipe(sourcemaps.init())
-    .pipe(concat([paths.buildNames.scripts])
+    .pipe(concat(paths.buildNames.scripts))
     .pipe(babel({
         presets: ['@babael/env']
     }))
-    .pipe(uglify())
+    .pipe(gulpif(process.env.NODE_ENV === 'production', uglify()))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.bulding.scripts));
 });
 
-gulp.task('taskcss', () => {
+gulp.task('css', () => {
     return gulp.src([paths.scr.styles])
     .pipe(sourcemaps.init())
-    .pipe(concat([paths.buildNames.styles])
-    .pipe(cssnano())
+    .pipe(concat(paths.buildNames.styles))
+    .pipe(gulpif(process.env.NODE_ENV === 'production', cssnano()))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.bulding.styles));
 });
 
-gulp.task('build', ['taskjs', 'taskcss'])
+gulp.task('build', ['js', 'css'])
 
 
 gulp.task('browser-sync', () => {
@@ -53,12 +58,12 @@ gulp.task('browser-sync', () => {
             baseDir: './'
         }
     })
-    gulp.watch(paths.scr.scripts, ['taskjs-watch']);
-    gulp.watch(paths.scr.styles, ['taskcss-watch']);
+    gulp.watch(paths.scr.scripts, ['js-watch']);
+    gulp.watch(paths.scr.styles, ['css-watch']);
 })
 
-gulp.task('taskjs-watch', ['js'], () => browserSync.reload());
-gulp.task('taskcss-watch', ['css'], () => browserSync.reload());
+gulp.task('js-watch', ['js'], () => browserSync.reload());
+gulp.task('css-watch', ['css'], () => browserSync.reload());
 
 gulp.task('prod', ['build']);
 gulp.task('dev', ['build', 'browser-sync']);
